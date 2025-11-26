e28a23c997ae928f2f8b90895ce3fde7b1f19384
WDEXcourse <wdkawasaki.online@gmail.com>
2025-11-26 19:11:19 +0900

    購入機能の追加

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function buy(){
+
+    var money=Number(document.querySelector(".stock_money").textContent)
+    var number=Number(document.querySelector(".stock_number").textContent)
+    alert('10株購入しました')
+    document.querySelector(".stock_number").textContent = number + 10;
+}
+function sell(){
+    alert('10株売りました')
+}
+
