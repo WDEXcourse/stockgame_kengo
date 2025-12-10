fbadcc134c6b86d4325820e6c7a520d4c94682df
WDEXcourse <wdkawasaki.online@gmail.com>
2025-12-10 19:06:14 +0900

    SKYのほぼ全容はできた。あとは株価の変動が必要。

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-function buy(){
-    var money=Number(document.querySelector(".stock_money").textContent);
-    var number=Number(document.querySelector(".stock_number").textContent);
-    alert('10株購入しました');
-    document.querySelector(".stock_number").textContent = number + 10;
+var Ownmoney=10000000;
+
+window.onload = function(){ 
+    document.querySelector(".possession_number").textContent = Ownmoney;
+
 }
 
-//できたら、株数がマイナスにならないようにしてほしい。
+function buy(){
+    var buystocks =Number(prompt("何株購入しますか？"));
+    
+    var money=Number(document.querySelector(".stock_money").textContent);
+    var number=Number(document.querySelector(".stock_number").textContent);
+    if(Ownmoney>=money*buystocks){
+    alert(buystocks+'株購入しました');
+    document.querySelector(".stock_number").textContent = number + buystocks;
+    Ownmoney=Ownmoney-money*buystocks
+    document.querySelector(".possession_number").textContent = Ownmoney;
+    }
+    else{
+        alert("購入できません")
+    }
+}
+
 function sell(){
-    alert('10株売りました');
+    
+    var money=Number(document.querySelector(".stock_money").textContent);
+    var number=Number(document.querySelector(".stock_number").textContent);
+
+    var sellstocks =prompt("何株売却しますか？")
+    if(sellstocks <=number){
+        alert(sellstocks+'株売りました');
+        document.querySelector(".stock_number").textContent = number - sellstocks; 
+        Ownmoney=Ownmoney+money*sellstocks;
+        
+        document.querySelector(".possession_number").textContent = Ownmoney;
+    }
+    else{
+        alert("売却できません")
+    }
 }
