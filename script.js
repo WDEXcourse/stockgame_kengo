90f5533e60bfcf54bfc60de94ad6787af9c1200f
WDEXcourse <86914835+WDEXcourse@users.noreply.github.com>
2025-12-17 19:07:55 +0900

    全ての株を定義、プログラムした

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
-var Ownmoney=10000000;
+var Ownmoney = 10000000;
+var stock_data = {
+    "SKY_STOCK": {
+        money: 500,
+        number: 0
+    },
+    "NITTA_STOCK": {
+        money: 1500,
+        number: 0
+    },
+    "TOYO_STOCK": {
+        money: 1000,
+        number: 0
+    },
+    "ISC_STOCK": {
+        money: 1300,
+        number: 0
+    }
 
-window.onload = function(){ 
-    document.querySelector(".possession_number").textContent = Ownmoney;
 
 }
 
-function buy(){
-    var buystocks =Number(prompt("何株購入しますか？"));
-    
-    var money=Number(document.querySelector(".stock_money").textContent);
-    var number=Number(document.querySelector(".stock_number").textContent);
-    if(Ownmoney>=money*buystocks){
-    alert(buystocks+'株購入しました');
-    document.querySelector(".stock_number").textContent = number + buystocks;
-    Ownmoney=Ownmoney-money*buystocks
+
+
+window.onload = function () {
     document.querySelector(".possession_number").textContent = Ownmoney;
-    }
-    else{
-        alert("購入できません")
-    }
+    generaize("SKY_STOCK")
+
 }
 
-function sell(){
-    
-    var money=Number(document.querySelector(".stock_money").textContent);
-    var number=Number(document.querySelector(".stock_number").textContent);
+function generaize(company) {
+    document.querySelector("#" + company + " .stock_money").textContent = stock_data[company].money;
+    document.querySelector("#" + company + " .stock_number").textContent = stock_data[company].number;
 
-    var sellstocks =prompt("何株売却しますか？")
-    if(sellstocks <=number){
-        alert(sellstocks+'株売りました');
-        document.querySelector(".stock_number").textContent = number - sellstocks; 
-        Ownmoney=Ownmoney+money*sellstocks;
-        
-        document.querySelector(".possession_number").textContent = Ownmoney;
-    }
-    else{
-        alert("売却できません")
-    }
+    var buy_button = document.querySelector(".buy_button");
+
+    buy_button.addEventListener('click', function () {
+        var buystocks = Number(prompt("何株購入しますか？"));
+        if (Ownmoney >= stock_data[company].money * buystocks) {
+            alert(buystocks + '株購入しました');
+
+            stock_data[company].number = stock_data[company].number + buystocks;
+            document.querySelector("#" + company + " .stock_number").textContent = stock_data[company].number;
+
+            Ownmoney = Ownmoney - stock_data[company].money * buystocks
+            document.querySelector(".possession_number").textContent = Ownmoney;
+        }
+        else {
+            alert("購入できません")
+        }
+    });
+
+    var sell_buutton = this.document.querySelector(".sell_button");
+    sell_buutton.addEventListener("click", function () {
+        var sellstocks = prompt("何株売却しますか？")
+        if (sellstocks <= stock_data[company].number) {
+            alert(sellstocks + '株売りました');
+            stock_data[company].number = stock_data[company].number - sellstocks
+            document.querySelector("#" + company + " .stock_number").textContent = stock_data[company].number;
+            Ownmoney = Ownmoney + stock_data[company].money * sellstocks;
+
+            document.querySelector(".possession_number").textContent = Ownmoney;
+        }
+        else {
+            alert("売却できません")
+        }
+
+    })
 }
